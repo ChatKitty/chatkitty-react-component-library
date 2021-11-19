5f999c63d9f6e44c280c27274d9d576097e6e95d
bowei <bowei.han100@gmail.com>
2021-11-19 16:44:18 -0500

    rollup configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 
 const packageJson = require("./package.json");
 
@@ -19,8 +21,15 @@ export default {
       format: "esm",
       sourcemap: true,
     },
+    {
+      file: packageJson.umd,
+      format: "umd",
+      name: "ChatKitty",
+      sourcemap: true,
+    },
   ],
   plugins: [
+    json(),
     peerDepsExternal(),
     resolve(),
     commonjs(),
@@ -28,5 +37,6 @@ export default {
     postcss({
       extensions: [".css"],
     }),
+    terser(),
   ],
 };
