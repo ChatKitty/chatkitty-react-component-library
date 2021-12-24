70626be24aabf2c77bb822cd06b8b0b02456feee
bowei <bowei.han100@gmail.com>
2021-12-24 13:11:22 -0500

    update stories

--- src/mocks.ts
+++ src/mocks.ts
@@ -26,13 +26,19 @@ export const MockChannels = [
   {
     name: "Toronto",
     description: "Toronto has a population of 3 million",
+    imageSrc:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQFEICiaNOZJ1w/profile-displayphoto-shrink_400_400/0/1632326497576?e=1642636800&v=beta&t=zCj6uU8mtR6K4Xv87FXJ4rGP4UmGB1YDiZl1iowKW68",
   },
   {
     name: "San Francisco",
     description: "San Francisco has a population of less than 1 million",
+    imageSrc:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQFEICiaNOZJ1w/profile-displayphoto-shrink_400_400/0/1632326497576?e=1642636800&v=beta&t=zCj6uU8mtR6K4Xv87FXJ4rGP4UmGB1YDiZl1iowKW68",
   },
   {
     name: "England",
     description: "England has a population of over 50 million",
+    imageSrc:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQFEICiaNOZJ1w/profile-displayphoto-shrink_400_400/0/1632326497576?e=1642636800&v=beta&t=zCj6uU8mtR6K4Xv87FXJ4rGP4UmGB1YDiZl1iowKW68",
   },
 ];
